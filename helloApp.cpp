0153d73c01f3d172773cfed93a8d7a0a8a08f034
chacthang <congtien.dinh19@gmail.com>
2025-06-24 03:44:13 +0700

    fist commit

---
+++ helloApp.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string.h>
+void sortMin(int *arr, size_t s){
+    int idx;
+
+    idx = *(arr+1)  ;
+    for (int k = 0; k < s; ++k) {
+        for (int i = 0; i < s ; ++i) {           
+            if (idx > *(arr + i)) {
+                idx = *(arr + i);
+            }
+            *(arr + k) = idx;
+        }
+    }
+
+    std::cout << "Min: "<<*arr << std::endl;
+}
+
+int main()
+{
+    int arr[5] = {3,20,8,21,6};
+    size_t size = sizeof(arr)/ sizeof(arr[0]);
+
+    sortMin(arr,size);
+
+    std::cout << "Hello World" << std::endl;
+
+    
+    // for (int i = 0; i < size ; ++i) {
+    //     std::cout << *(arr+i) << std::endl;
+    // }
+    
+}
