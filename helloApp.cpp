19f70037526581c60790bf58149bae976ea4c24b
chacthang <congtien.dinh19@gmail.com>
2025-06-25 00:47:02 +0700

    sortMax addition

--- helloApp.cpp
+++ helloApp.cpp
@@ -1,39 +1,69 @@
 #include <iostream>
 #include <string.h>
-void sortMin(int *arr, size_t s){
-    int idx;
-
-    idx = *(arr+1)  ;
-    for (int k = 0; k < s; ++k) {
-        for (int i = 0; i < s ; ++i) {           
-            if (idx > *(arr + i)) {
-                idx = *(arr + i);
-            }
-            *(arr + k) = idx;
-        }
-    }
-
-    std::cout << "Min: "<<*arr << std::endl;
-}
+#include <chrono>
+#include <windows.h>
+#include <psapi.h>
+void sortMin(int *arr, size_t s);
+void sortMax(int *arr, size_t s);
 
 int main()
 {
+    auto start = std::chrono::high_resolution_clock::now();
     int arr[5] = {3,20,8,21,6};
     size_t size = sizeof(arr)/ sizeof(arr[0]);
 
     sortMin(arr,size);
-<<<<<<< HEAD
-    std::cout << "Hello from local city" << std::endl;
-    std::cout << "Hello World" << std::endl;
-    std::cout << "Hello World 123" << std::endl;
-=======
+    //std::cout << "Hello World" << std::endl;
 
-    std::cout << "Hello World 123" << std::endl;
->>>>>>> feature-123
 
-    
-    // for (int i = 0; i < size ; ++i) {
-    //     std::cout << *(arr+i) << std::endl;
-    // }
-    
+    for (int i = 0; i < size ; ++i) {
+        std::cout << *(arr+i) << " ";
+    }
+    std::cout << std::endl;
+    sortMax(arr, size);
+    for (int i = 0; i < size ; ++i) {
+        std::cout << *(arr+i) << " ";
+    }
+
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> duration = end - start; 
+
+    std::cout << "Runtime: " << duration.count() << " ms" << std::endl;
+
+    PROCESS_MEMORY_COUNTERS pmc;
+    GetProcessMemoryInfo(GetCurrentProcess(), &pmc, sizeof(pmc));
+    std::cout << "Memory usage: " << pmc.WorkingSetSize / 1024 << " KB\n";
+}
+
+void sortMin(int *arr, size_t s){
+    int idx;
+    idx = *(arr)  ;
+    for (int k = 0; k < s; ++k) {
+        idx = *(arr+k)  ;
+        for (int i = k; i < s ; ++i) {           
+            if (idx > *(arr + i)) {
+                idx = *(arr + i);
+                *(arr + i) = *(arr + k);
+                *(arr + k) = idx;
+            }
+           // *(arr + k) = idx;
+        }
+    }
+
+}
+
+void sortMax(int *arr, size_t s) {
+    int idx;
+    idx = *(arr)  ;
+    for (int k = 0; k < s; ++k) {
+        idx = *(arr+k)  ;
+        for (int i = k; i < s ; ++i) {           
+            if (idx < *(arr + i)) {
+                idx = *(arr + i);
+                *(arr + i) = *(arr + k);
+                *(arr + k) = idx;
+            }
+           // *(arr + k) = idx;
+        }
+    }
 }
